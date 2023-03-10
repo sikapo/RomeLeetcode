2636cdce11299638e378592d37bbb38a571af0ba
Owner <vitycity79@gmail.com>
2023-03-10 15:20:48 +0200

    rome v2

--- src/Rome.java
+++ src/Rome.java
@@ -5,7 +5,6 @@ public class Rome {
         String enterNumber; // M CM XC IV 1994
         char[] rome = {'I', 'V', 'X', 'L', 'C', 'D', 'M'};
         int[] arabic = {1, 5, 10, 50, 100, 500, 1000};
-        int amount = 0;
 
         Scanner scanner = new Scanner(System.in);
         do {
@@ -14,14 +13,24 @@ public class Rome {
         } while (enterNumber.length() == 0);
 
         char[] sArray = enterNumber.toCharArray();
+        int [] sArrayForInt = new int [sArray.length];
 
         for (int i = 0; i < 7; i++) {
             for (int ii = 0; ii < sArray.length; ii++) {
                 if (sArray [ii] == rome [i]) {
-                    amount += arabic [i];
+                    sArrayForInt[ii] = arabic [i];
                 }
             }
         }
-        System.out.println(amount);
+
+        int finalInteger = 0;
+        for (int i = 0; i <  sArrayForInt.length; i++) {
+            if ((sArrayForInt[i] - sArrayForInt[i + 1]) < 0){
+                finalInteger = finalInteger + ((sArrayForInt[i] - sArrayForInt[i + 1]) * - 1);
+                i++;
+            }
+            else finalInteger += sArrayForInt[i];
+        }
+        System.out.println(finalInteger);
     }
 }
